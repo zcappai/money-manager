6c702b57444ad317b118aba34f5d66e1770b40df
DELL-XPS-15\Akhilesh Pai <akpai22@googlemail.com>
2023-07-15 12:01:37 +0100

    Adding nodemon to server to refresh server on save, added error printing to server, and now app successfully fetches access token from Monzo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+type AuthorisationType = {
+	accessToken: string | null | null;
+	clientID: string | null;
+	expiresIn: number | null;
+	scope: string | null;
+	tokenType: string | null;
+	userID: string | null;
+};
 
 function App() {
 	const [monzoAuthURL, setMonzoAuthURL] = useState<string | null>(null);
 	const [monzoCode, setMonzoCode] = useState<string | null>(null);
+	const [authorisationData, setAuthorisationData] = useReducer(
+		(curData: AuthorisationType, newData: Partial<AuthorisationType>) => ({
+			...curData,
+			...newData,
+		}),
+		{
+			accessToken: null,
+			clientID: null,
+			expiresIn: null,
+			scope: null,
+			tokenType: null,
+			userID: null,
+		}
+	);
 
 	useEffect(() => {
-		fetch("/api")
-			.then((res) => res.text())
-			.then(setMonzoAuthURL);
+		fetch("/api/get_auth_url")
+			.then((res) => res.json())
+			.then((val) => setMonzoAuthURL(val.authURL));
 	}, []);
 
 	useEffect(() => {
@@ -18,6 +41,23 @@ function App() {
 		setMonzoCode(code);
 	}, []);
 
+	useEffect(() => {
+		if (monzoCode && !authorisationData.accessToken) {
+			fetch(`/api/exchange_auth_code?authCode=${monzoCode}`)
+				.then((res) => res.json())
+				.then((val) =>
+					setAuthorisationData({
+						accessToken: val.access_token,
+						clientID: val.client_id,
+						expiresIn: val.expires_in,
+						scope: val.scope,
+						tokenType: val.token_type,
+						userID: val.user_id,
+					})
+				);
+		}
+	}, [authorisationData.accessToken, monzoCode]);
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -25,7 +65,7 @@ function App() {
 				{monzoCode}
 				<button
 					onClick={() =>
-						window.location.replace(monzoAuthURL ? monzoAuthURL : "")
+						window.open(monzoAuthURL ? monzoAuthURL : "", "_blank")
 					}
 				>
 					Authorise Monzo
--- server/index.ts
+++ server/index.ts
@@ -1,45 +1,51 @@
 import express from "express";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const PORT = process.env.PORT || 3001;
 const monzoAuthUrl = "https://auth.monzo.com";
 const monzoApiUrl = "https://api.monzo.com";
+const redirectURL = "http://localhost:3000";
 
 const app = express();
 
 const oauthDetails = {
 	client_id: "oauth2client_0000AXYfpUynUsrUvkqM2E",
-	client_secret: "[your client secret]",
+	client_secret:
+		"mnzpub.54oESFAw5DsTzw3TYnxLF4iQwNqn9mJlMP3GcruHLIO6lbLHYkQ0fgh65/Ng3haFtAvArWqrcXEbwgMCKd6AuA==",
 	redirect_uri: `http://localhost:${PORT}/oauth/callback`,
 };
 
 let tempAuthCode: string;
 
-app.get("/api", (req, res) => {
+app.get("/api/get_auth_url", (req, res) => {
 	const { client_id, redirect_uri } = oauthDetails;
-
-	res.type("html");
-	res.send(
-		`${monzoAuthUrl}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`
-	);
+	let authURL = `${monzoAuthUrl}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
+	res.json({ authURL: authURL });
 });
 
 app.get("/oauth/callback", (req, res) => {
 	const { code } = req.query;
 	if (code && typeof code === "string") {
 		tempAuthCode = code;
-		res.redirect(`/api?code=${code}`);
+		res.redirect(`${redirectURL}?code=${code}`);
 	}
 });
 
 app.get("/api/exchange_auth_code", (req, res) => {
-	axios.post(`${monzoApiUrl}/oauth2/token`, {
-		grant_type: tempAuthCode,
-		client_id: "client_id",
-		client_secret: "client_secret",
-		redirect_uri: "redirect_uri",
-		code: "authorization_code",
-	});
+	const { authCode } = req.query;
+	const { client_id, client_secret, redirect_uri } = oauthDetails;
+
+	const formData = new URLSearchParams();
+	formData.set("grant_type", "authorization_code");
+	formData.set("client_id", client_id);
+	formData.set("client_secret", client_secret);
+	formData.set("redirect_uri", redirect_uri);
+	formData.set("code", authCode as string);
+
+	axios
+		.post(`${monzoApiUrl}/oauth2/token`, formData.toString())
+		.then((result) => res.json(result.data))
+		.catch(printError);
 });
 
 app.get("/api/ping_monzo", (req, res) =>
@@ -51,3 +57,22 @@ app.get("/api/ping_monzo", (req, res) =>
 );
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+const printError = (error: AxiosError) => {
+	if (error.response) {
+		// The request was made and the server responded with a status code
+		// that falls out of the range of 2xx
+		console.log(error.response.data);
+		console.log(error.response.status);
+		console.log(error.response.headers);
+	} else if (error.request) {
+		// The request was made but no response was received
+		// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+		// http.ClientRequest in node.js
+		console.log(error.request);
+	} else {
+		// Something happened in setting up the request that triggered an Error
+		console.log("Error", error.message);
+	}
+	console.log(error.config);
+};
