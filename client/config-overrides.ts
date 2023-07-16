37236be572e2a2234097afd34e65965ae7f2577f
DELL-XPS-15\Akhilesh Pai <akpai22@googlemail.com>
2023-07-16 14:23:36 +0100

    Adding and configuring salesforce lightning design system

---
+++ client/config-overrides.ts
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = function override(config, env) {
+	config.module.rules = [
+		// salesforce dependencies
+		// this will compile salesforce lightning as src, not as package
+		{
+			test: /\.tsx?$/,
+			include: ["node_modules/@salesforce/design-system-react"].map((someDir) =>
+				path.resolve(process.cwd(), someDir)
+			),
+			loader: require.resolve("babel-loader"),
+			options: {
+				presets: ["react-app"],
+			},
+		},
+	].concat(config.module.rules);
+
+	return config;
+};
